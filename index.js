01a2e33d3ab4e16a873ef4426896bdebd74e6ac9
stephen talley <stvlley.amzn@gmail.com>
2022-02-07 10:23:25 -0600

    completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+// break tutorials array into smaller pieces
+//  cut each string .map()
+// cut the string into words
+// index
+
+
 
 const titleCased = () => {
-  return tutorials
+  let breakTheArray = tutorials.map(toTitle)
+  return breakTheArray
 }
+
+function toTitle(string) {
+  let stringSplit = string.split(' ') 
+  console.log(stringSplit)
+  let breakTheString = stringSplit.map(element => element.charAt(0).toUpperCase() + element.slice(1));
+  console.log(breakTheString.join(' '))
+  return breakTheString.join(' ')
+  
+}
+
+toTitle('implementing Blockchain Web API')
+
+
+
+
+// let string = "this is a string"
+
+// let substrings = string.toUpperCase().split()
+
+
+// console.log(substrings)
+
